fb6dbeccf6e32462fe02917070c283ebf7996dcc
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-08-09 09:08:16 -0700

    BUILD

--- build/index.js
+++ build/index.js
@@ -2475,7 +2475,7 @@ module.exports = {
 		"test": "nn -s;mocha -u tdd --compilers coffee:coffee-script/register",
 		"testInBrowser": "webpack-dev-server --progress"
 	},
-	"version": "1.13.0"
+	"version": "2.0.0"
 };
 
 /***/ }),
